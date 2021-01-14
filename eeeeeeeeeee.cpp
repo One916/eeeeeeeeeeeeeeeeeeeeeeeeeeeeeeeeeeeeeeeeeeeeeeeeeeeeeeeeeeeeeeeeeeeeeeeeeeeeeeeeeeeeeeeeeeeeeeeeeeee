966e26ec2f2d70c88991195a0be1c4a60f699e88
One916 <64084525+One916@users.noreply.github.com>
2021-01-14 20:12:04 +0800

    eeeeeeeeeeeeeeeeeeee

---
+++ eeeeeeeeeee.cpp
@@ -0,0 +1,21 @@
+#define ee <iostream>
+#define eee int main(){int
+#define eeeee do{
+#define eeeeee while
+#define eeeeeee cout<<(char)e;
+#define eeee ;}
+#include ee
+eee e;
+e=e^e;//0
+e=!e;//1
+e*=(e+e);//2
+e*=e*e;//8
+e*=e;//64
+e+=(e/(e/e+e/e));//96
+e+=(e/((e/e+e/e)*(e/e+e/e))/(e/e+e/e)/(e/e+e/e+e/e));//100
+e+=e/e;//101
+eeeee
+eeeeeee
+eeee
+eeeeee((e/e)&&e==e)
+eeee
